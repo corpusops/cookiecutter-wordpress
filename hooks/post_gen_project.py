a22f0c31bbcae334dc565da99a89579d17532fcc
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2019-02-12 00:55:41 +0100

    style

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -122,6 +122,7 @@ while read f;do
         "$f"
     fi
 done < <( find -type f|egrep -v "((^./(\.tox|\.git|local))|/static/)"; )
+sed -i -re "/\/code\/sys\/\* sys/d" Dockerfile
 {% endif %}
 set -x
         """
